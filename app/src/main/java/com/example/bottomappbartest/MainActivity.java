45e454ce692ff7629fa3cecfc0d391d2fc1f4601
Ceyhan ILERI <ceyhan.ileri@airties.com>
2019-12-13 13:41:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bottomappbartest/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.bottomappbartest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomAppBar bottomAppBar;
+    private BottomSheetDialog bottomSheetDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomAppBar = findViewById(R.id.bottom_app_bar);
+        bottomAppBar.replaceMenu(R.menu.bottomappbar_menu);
+        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNavigationMenu();
+            }
+        });
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.app_bar_fav:
+                        Toast.makeText(MainActivity.this, "fav clicked.", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.app_bar_search:
+                        Toast.makeText(MainActivity.this, "search clicked.", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.app_bar_settings:
+                        Toast.makeText(MainActivity.this, "settings clicked.", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.sandwich_menu:
+                        Toast.makeText(MainActivity.this, "settings clicked.", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void openNavigationMenu() {
+
+        //this will get the menu layout
+        final View bootomNavigation = getLayoutInflater().inflate(R.layout.navigation_menu,null);
+        bottomSheetDialog = new BottomSheetDialog(MainActivity.this);
+        bottomSheetDialog.setContentView(bootomNavigation);
+        bottomSheetDialog.show();
+
+        //this will find NavigationView from id
+        NavigationView navigationView = bootomNavigation.findViewById(R.id.navigation_menu);
+
+        //This will handle the onClick Action for the menu item
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                switch (id){
+                    case R.id.item1:
+                        Toast.makeText(MainActivity.this,"Item 1 Clicked",Toast.LENGTH_SHORT).show();
+                        bottomSheetDialog.dismiss();
+                        break;
+                    case R.id.item2:
+                        Toast.makeText(MainActivity.this,"Item 2 Clicked",Toast.LENGTH_SHORT).show();
+                        bottomSheetDialog.dismiss();
+                        break;
+                    case R.id.item3:
+                        Toast.makeText(MainActivity.this,"Item 3 Clicked",Toast.LENGTH_SHORT).show();
+                        bottomSheetDialog.dismiss();
+                        break;
+                    case R.id.item4:
+                        Toast.makeText(MainActivity.this,"Item 4 Clicked",Toast.LENGTH_SHORT).show();
+                        bottomSheetDialog.dismiss();
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+}
